563c0b9a7379623a1e4abec33561c791c5f784ce
Takuma Mizuno <34719035ict.nitech.ac.jp>
2024-08-15 14:10:55 +0900

    first

---
+++ language.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 25 11:22:12 2024
+
+@author: tack1
+"""
+
+import numpy as np
+import csv
+
+def getData(data_name):
+    ngram_data = {}
+    with open(data_name, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            ngram, count = row
+            ngram_data[ngram] = int(count)
+    return ngram_data
+
+
+if __name__ == "__main__":
+
+    # CSVを読み込む
+    data1 = getData("1.csv")
+    data2 = getData("2.csv")
+    data3 = getData("3.csv")
+    data4 = getData("4.csv")
+
+    # データ部を取得
+    print(data1)
+    print(data2)
+    print(data3)
+    print(data4)
+
